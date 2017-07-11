fced9d833cc3391ce09c4fc4e531a51884ca27b4
Sarah Heim <scheim@ucsd.edu>
2017-07-11 09:47:53 -0700

    Limited well data toFixed(2). For temperature: show degrees in Celsius AND Fahrenheit. Also changed newline type of file.

--- sass_latest.js
+++ sass_latest.js
@@ -18,7 +18,7 @@ $.each(flagsArr, function(f, fAttr) {
 });
 var allArr = attrsArr.concat(psFlagsArr);
 
-var stationsMeta = { 
+var stationsMeta = {
 /*
 //Bright colors
   'UCSB': { 'border-color':'#7700FF', 'fill-color':'#CCCCFF'},
@@ -65,10 +65,10 @@ function functionName(data){
 			style: { opacity: 0.8 },
 			labelStyle: { fontSize: '300%' }
 		},
-		xAxis: { 
+		xAxis: {
 			type: 'datetime',
 			ordinal: false,
-			labels : { 
+			labels : {
 				format: '{value:%m-%d %H:%M}',
 				rotation: 20
 			}
@@ -92,15 +92,15 @@ function functionName(data){
 	        inputEnabled: false
 	    },
 //            navigator: { enabled: false },
-	    scrollbar: { enabled: false 
+	    scrollbar: { enabled: false
   //              , liveRedraw:false
             },
 	    // title : { text : 'Title' },
 	    plotOptions: {
-                  line: { 
+                  line: {
                       pointInterval: 60 * 1000,
-                      gapSize: 30 
-                  }, 
+                      gapSize: 30
+                  },
 			series: {
 
                                 lineWidth: 1,
@@ -114,7 +114,7 @@ function functionName(data){
                                         // xDateFormat: '%A %m-%d-%Y %H:%M'
                                         headerFormat: ''
 					// pointFormatter: tooltipFlags
-				}, 
+				},
 			   	turboThreshold: 0 //CRUCIAL for more than 1000 points!!!!
 			}
 	    },
@@ -126,7 +126,7 @@ function functionName(data){
 	    			// console.log('HIDE loading');
 	    		// }
 	    	// }
-	    	// , point: { 
+	    	// , point: {
 	    		// events: {
 	    			// mouseOver: function () {
 	    				// console.log(this);
@@ -143,7 +143,7 @@ function functionName(data){
     	// item[0] = new Date(item[0]).getTime();
     	/// get min and max from data to be used to set yAxis range
     	if (item[2] < min) { min = item[2] }
-    	if (item[2] > max) { max = item[2] } 
+    	if (item[2] > max) { max = item[2] }
     	row = {
     		x: new Date(item[1]).getTime(),
     		y: item[2],
@@ -197,7 +197,7 @@ function getDataUrl(){
     if ($("#onlyQC").prop('checked')) {
          // jsonUrl += '&'+attr+'_flagPrimary=1';
          if (flagsArr.indexOf(attr) > -1) urlEnd  += '&'+attr+'_flagPrimary=1';
-    } else if (flagsArr.indexOf(attr) > -1) { 
+    } else if (flagsArr.indexOf(attr) > -1) {
          // jsonUrl += ','+attr+'_flagPrimary'+','+attr+'_flagSecondary';
          urlEnd  += ','+attr+'_flagPrimary'+','+attr+'_flagSecondary';
     }
@@ -216,7 +216,7 @@ function getDataUrl(){
 function getData() {
     attr = $("#attr").val();
     $('#compare').attr('href', './timeline/?main=multi&attr='+attr);
-    if (chart) chart.showLoading(); 
+    if (chart) chart.showLoading();
 	//$('#loader').show();
     url = getDataUrl();
     // console.log('getData(url):', url);
@@ -228,15 +228,15 @@ function getData() {
 	//    jsonpCallback: 'functionName',
         // crossDomain: true,
         success: function(dataWeGotViaJsonp){
-          window['functionName'](dataWeGotViaJsonp);      
+          window['functionName'](dataWeGotViaJsonp);
           chart.hideLoading();
           $('#loader').hide();
           // setStation();
         },
 		error:function(){
 			alert("Error");
-		}    
-    });	
+		}
+    });
 }
 
 function getStationDate(url, callbackFun) {
@@ -304,7 +304,7 @@ function createLatestWells() {
                    '<span id="'+dataId+'_time"></span><table id="'+
                     dataId+'" class="table table-sm table-hover"></table>');
 
- 
+
      $("#latest").append(staDiv);
 
 //     latestUrl = baseJSON+'?station,time,'+attrsArr.join(',')+'&station="'+sta+'"&orderByMax(%22time%22)';
@@ -320,8 +320,8 @@ function createLatestWells() {
           success: function(returnedData){
             latestDataFun(returnedData);
           },
-          error:function(xhr, status, error){ 
-            console.log("ERROR:", sta); 
+          error:function(xhr, status, error){
+            console.log("ERROR:", sta);
             console.log(xhr.responseText, status, error);
             return false;
           }
@@ -347,12 +347,12 @@ function latestDataFun(latest) { //data is passed
   //make time line/span
   var latestDT = new Date(staDic['time']['value']).getTime();
   var diffNowMins = (now - latestDT)/ (60*1000);
-  if (diffNowMins < 30) { 
+  if (diffNowMins < 30) {
     // timeClass = 'label label-success';
     timeClass = '';
     timeIcon = '';
-  } else { 
-    // timeClass = 'label label-danger'; 
+  } else {
+    // timeClass = 'label label-danger';
     timeClass = 'text-danger';
     timeIcon = '<span class="glyphicon glyphicon-time" title="< 30 mins old"></span>';
   }
@@ -375,8 +375,9 @@ function latestDataFun(latest) { //data is passed
   tableBody = $('#'+dataId).append(document.createElement("tbody"));
 
   $.each(attrsArr , function(a, attr) {
-    valTxt = staDic[attr]['value'] +' '+ staDic[attr]['unit']
-    //ATTRIBUTE HAS FLAG
+    valFx = staDic[attr]['value'].toFixed(2);
+    valTxt = valFx +' '+ staDic[attr]['unit']
+    //ATTRIBUTE HAS FLAG, add icon if flag is not "good"
     if (flagsArr.indexOf(attr) > -1) {
       pFlag = staDic[attr+'_flagPrimary']['value']
       flagTxt = '(FLAGS: '+pFlag+', '+staDic[attr+'_flagSecondary']['value']+')'
@@ -384,17 +385,15 @@ function latestDataFun(latest) { //data is passed
       else { icon = '' }
     } else { icon = ''}
 
-/*
-    var dataSpan = document.createElement("span");
-    dataSpan.setAttribute('id', 'latestSpan_'+sta+'_'+attr);
-//    dataSpan.innerHTML = attr+': <button type="button" id="latest_'+sta+'_'+attr+'" class="'+className+'">'+valTxt+'</button><br>';
-    dataSpan.innerHTML = attr+': <button type="button" id="latest_'+sta+'_'+attr+'" class="btn btn-compact btn-link">'+valTxt+'</button>'+icon+'<br>';
-    if (flagsArr.indexOf(attr) == -1) dataSpan.setAttribute('style','display:none'); 
-    $('#'+dataId).append(dataSpan);
-*/
     var tableRow = document.createElement("tr");
     tableRow.setAttribute('id', 'latestSpan_'+sta+'_'+attr);
     if (flagsArr.indexOf(attr) == -1) tableRow.setAttribute('class', 'collapse out');
+    //Show temperature as °C & °F
+    if (attr == 'temperature') {
+      dC = staDic[attr]['value'];
+      dF = (dC * 9 / 5 + 32).toFixed(2);
+      valTxt = valFx +'&deg;C / '+dF+'&deg;F'
+    }
     tableRow.innerHTML = '<th scope "row">'+attr+'</th><td>'+valTxt+' '+icon+'</td>'
     tableBody.append(tableRow);
   });
@@ -438,7 +437,7 @@ function getStationStyle(sta) {
 //  $('#timeline_'+sta).css('background-image', 'linear-gradient(to bottom,'+fColor+' 30%,'+bColor+' 150%)');
   $(timelineId).css('background', moreBColor);
   $(timelineId).css('color', 'white');
-  
+
 
 //  $(timelineId+':hover'+', '+timelineId+':focus').css('background', 'red');
 
@@ -455,7 +454,7 @@ function getStationStyle(sta) {
   staActive.innerHTML = '#'+'timeline_'+sta+'.active { color: black !important; background: '+moreFColor+' !important; }';
   document.head.appendChild(staActive);
 
-  $(timelineId).hover(function () { 
+  $(timelineId).hover(function () {
     //console.log('entering', $(this).attr('id'));
     $(this).addClass($(this).attr('id')+'_hover');
   }, function() {
@@ -491,7 +490,7 @@ $(function () {
   });
   //'.table > tbody > tr'
   //'.table.table-sm.table-hover'
-  $(document).on('click', ".table.table-hover > tbody > tr", function() { 
+  $(document).on('click', ".table.table-hover > tbody > tr", function() {
 //    console.log('doc click row: ', $(this).attr('id'));
     selBtnId = $(this).attr('id');
     selBtnIdArr = selBtnId.split('_');
@@ -509,15 +508,15 @@ $(function () {
     }
 
   });
-  
+
   sensor = $('.btn-group > .btn.active > input').val();
   // console.log($('.btn-group > .btn.active > input').val());
   // getData();
   setStation();
 
-  $('#toggleFlag').click(function(){ 
+  $('#toggleFlag').click(function(){
     $('#collapseFlag').toggle();
-    return false; 
+    return false;
   });
 
   //~~~~~~~~~~~~~~~~~~~~Map ~~~~~~~~~~~~~~~~~~~~
@@ -581,14 +580,14 @@ $(function () {
   });
 
   var element = document.getElementById('popup');
-  
+
   var popup = new ol.Overlay({
     element: element,
     positioning: 'bottom-center',
     stopEvent: false
   });
   map.addOverlay(popup);
-  
+
   // display popup on click
   map.on('click', function(evt) {
     var feature = map.forEachFeatureAtPixel(evt.pixel,
@@ -608,8 +607,6 @@ $(function () {
         'content': feature.get('description')+'<br>'+hdms
       });
       $(element).popover('show');
-    } 
+    }
   });
 });
-
-
